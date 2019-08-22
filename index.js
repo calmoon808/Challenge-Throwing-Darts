c66c1e861294e9434cbfa750eaa98e4acd05f52a
Calvin <calmoon808@gmail.com>
2019-08-21 18:19:56 -1000

    darts

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 function scoreCalculator(arr) {
   // do work here
+  let isLessThanFive = true;
+  let score = 0;
+  for (let i = 0, n = arr.length; i < n; i++){
+    if (arr[i] >= 5){
+      isLessThanFive = false;
+    };
+    if (arr[i] >= 5 && arr[i] <= 10){
+      score += 5;
+    } else if (arr[i] < 5){
+      score += 10;
+    };
+  }
+  if (isLessThanFive === true){
+    score += 100;
+  }
+  return score;
 }
+
+console.log(scoreCalculator([1, 5, 11]));
+console.log(scoreCalculator([1, 2, 1, 4, 4, 2]))
 
 module.exports = scoreCalculator;
--- test/spec.js
+++ test/spec.js
@@ -1,1 +1,11 @@
 // write tests here
+const expect = chai.expect;
+const assert = chai.assert;
+
+describe("scoreCalculator", () => {
+    describe("isFunction", () => {
+        it("should be a function", () => {
+            expect(scoreCalculator).to.be.a("function");
+        })
+    })
+});
